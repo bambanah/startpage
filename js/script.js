99cc95d20fd110c064322e5ba6d4a21625006e51
Lachie <lachlanu@gmail.com>
2017-04-15 19:27:29 +1000

    First Draft - Completed main stylesheet

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function testing() {
+  var addressBar=document.getElementById('urlbar');
+  addressBar.focus();
+}
+
+function testing2() {
+
+}
+
+function start() {
+  testing();
+  testing2();
+}
+
+window.onload = start;
