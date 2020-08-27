3f77aa4440f9a9d54d83cd2541111b8e7fbcc747
Lachlan Underhill <lachlanu@gmail.com>
2020-08-27 21:31:24 +1000

    Remove some redundant comments

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -2,16 +2,13 @@ import firebase from "gatsby-plugin-firebase";
 import { getCurrentUserId } from "./auth";
 
 export const assignDefaultLinks = (userId: string) => {
-  // Get the predefined default links from the json
   const defaultLinks = require("./default-links.json");
 
-  // Push the default object to firebase
   firebase.database().ref(`/users/${userId}`).set(defaultLinks);
 };
 
 export const updateLinks = (links: any) => {
   // TODO: Define an interface for the links object as saved in the database (replace any!)
-  // Get user id of currently logged in user
   const userId = getCurrentUserId();
 
   // Push new link object - typescript will ensure format of object is 'legal'
