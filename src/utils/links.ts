9c70aa861f08ec9d556f9cd892feda2113ab32e2
Lachlan Underhill <lachlanu@gmail.com>
2020-08-28 16:33:01 +1000

    Removed 'any' type spec for link data in updateLinks function

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,14 +1,15 @@
 import firebase from "gatsby-plugin-firebase";
 import { getCurrentUserId } from "./auth";
 
+import { Data } from "./types";
+
 export const assignDefaultLinks = (userId: string) => {
   const defaultLinks = require("./default-links.json");
 
   firebase.database().ref(`/users/${userId}`).set(defaultLinks);
 };
 
-export const updateLinks = (links: any) => {
-  // TODO: Define an interface for the links object as saved in the database (replace any!)
+export const updateLinks = (links: Data) => {
   const userId = getCurrentUserId();
 
   // Push new link object - typescript will ensure format of object is 'legal'
